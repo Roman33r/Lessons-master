1d6faf9099109be3266c37ae6a9138bdc1b1b034
TopAcademy <zolotarenko46@gmail.com>
2024-03-15 21:11:19 +0300

    фыв

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import os
 from flask import Flask, render_template, request, g, flash, abort, session, redirect, url_for, make_response
 from usefull.FDataBase import FDataBase
 from usefull.UserLogin import UserLogin
+from usefull.forms import LoginForm, RegisterForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_user, login_manager, login_required, logout_user, current_user
+
+
 
 app = Flask(__name__)
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'test.db')))
 app.config["SECRET_KEY"] = "hoirjghojropgjehueEFGEOKOPje"
-app.config["MAX_CONTENT_LENGTH"] = 3 * 1024 * 1024#Максимальный размер файлов для загрузки на сервер
+app.config["MAX_CONTENT_LENGTH"] = 3 * 1024 * 1024 #Максимальный размер файлов для загрузки на сервер
 
 dbase = None
 
@@ -46,38 +49,61 @@ def create_db():
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('profile'))
-    if request.method == "POST":
-        user = dbase.getUserByEmail(request.form['email'])
-        if user and check_password_hash(user['psw'], request.form['psw']):
+    # if request.method == "POST":
+    #     user = dbase.getUserByEmail(request.form['email'])
+    #     if user and check_password_hash(user['psw'], request.form['psw']):
+    #         userlogin =  UserLogin().create(user)
+    #         rem = True if request.form.get('remainme') else False
+    #         login_user(userlogin, remember=rem)
+    #         return redirect(request.args.get('next') or url_for('profile'))
+    #     flash("Неверные данные - логин или пароль")
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = dbase.getUserByEmail(form.email.data)
+        if user and check_password_hash(user['psw'], form.psw.data):
             userlogin =  UserLogin().create(user)
-            rem = True if request.form.get('remainme') else False
+            rem = form.remember.data
             login_user(userlogin, remember=rem)
             return redirect(request.args.get('next') or url_for('profile'))
-        flash("Неверные данные - логин или пароль")
-    return render_template("login.html",menu=dbase.getMenu(), title = "Авторизация")
+        flash("Неверные данные - логин или пароль", "error")
+
+    return render_template("login.html", menu=dbase.getMenu(), title="Авторизация", form = form)
+    # return render_template("login.html",menu=dbase.getMenu(), title = "Авторизация")
 
 
 
 @app.route("/register", methods=["POST","GET"])
 def register():
-    if request.method == "POST":
-        # session.pop('_flashes',None)
-        if len(request.form['name']) > 4 \
-                and len(request.form['email'])>4 \
-                and len(request.form['psw'])>4 \
-                and request.form['psw'] == request.form['psw2']:
-
-            hash = generate_password_hash(request.form['psw'])
-            res = dbase.addUser(request.form['name'],request.form['email'],hash)
+    form = RegisterForm()
+    if form.validate_on_submit():
+            hash = generate_password_hash(form.psw.data)
+            res = dbase.addUser(form.name.data,form.email.data,hash)
             if res:
                 flash("Вы успешно зарегистрированы","success")
                 return redirect(url_for('login'))
             else:
                 flash("Ошибка при добавлении в БД","error")
-        else:
-            flash("Неверно заполнены поля","error")
 
-    return render_template("register.html",menu=dbase.getMenu(), title = "Регистрация")
+    return render_template("register.html", menu=dbase.getMenu(), title="Регистрация", form=form)
+
+    # if request.method == "POST":
+    #     # session.pop('_flashes',None)
+    #     if len(request.form['name']) > 4 \
+    #             and len(request.form['email'])>4 \
+    #             and len(request.form['psw'])>4 \
+    #             and request.form['psw'] == request.form['psw2']:
+    #
+    #         hash = generate_password_hash(request.form['psw'])
+    #         res = dbase.addUser(request.form['name'],request.form['email'],hash)
+    #         if res:
+    #             flash("Вы успешно зарегистрированы","success")
+    #             return redirect(url_for('login'))
+    #         else:
+    #             flash("Ошибка при добавлении в БД","error")
+    #     else:
+    #         flash("Неверно заполнены поля","error")
+
+
 
 # Routes
 @app.route("/")
